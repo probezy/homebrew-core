78e7a320448d87dcc1dda26e0e0f6f236300f6d8
Michael <probezy@gmail.com>
2019-12-07 18:55:57 +0800

    add new formula cpolar

---
+++ Formula/cpolar.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class CpolarStableDarwinAmd < Formula
+  desc ""
+  homepage ""
+  url "https://www.cpolar.com/static/downloads/cpolar-stable-darwin-amd64.tar.gz"
+  sha256 "baec7f205b6810d44616bf3b54416291505e9d3c66ba9b80c5524417d25517c4"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    #system "./configure", "--disable-debug",
+    #                        "--disable-dependency-tracking",
+    #                      "--disable-silent-rules",
+    #                      "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test cpolar-stable-darwin-amd`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
